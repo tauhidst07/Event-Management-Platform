f8c20e2bbb1bbeb19238b7ba9318d765b1054dd8
tauhidst07 <atauhid07@gmail.com>
2024-10-21 14:06:11 +0530

    add authMiddleware

---
+++ backend/Middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken"); 
+require('dotenv').config();
+const authMiddleware = async(req,res,next)=>{
+    if(!req.headers.authorization){
+        return res.status(400).json({
+            msg:"header is reuiqred"
+        }) 
+    } 
+    const token = req.headers.authorization.split(" ")[0]; 
+    try{
+       const decode = jwt.verify(token,process.env.JWT_SECRET);  
+       console.log(decode);
+       req.email=decode.userEmail; 
+       req.role=decode.role; 
+       next();
+    } 
+    catch(err){
+       return res.status(400).json({
+        msg:"invalid token"
+       })
+    }
+    
+} 
+
+module.exports=authMiddleware;
