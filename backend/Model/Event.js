d331c05d68476eeeb42f0ec466e1d95b3e8a21f8
tauhidst07 <atauhid07@gmail.com>
2024-10-21 15:31:13 +0530

    add event creation route

--- backend/Model/Event.js
+++ backend/Model/Event.js
@@ -11,7 +11,7 @@ const eventSchema = mongoose.Schema({
         required:true
     }, 
     date:{
-        type:String, 
+        type:Date, 
         required:true
     }, 
     createdBy:{
--- backend/Routes/Admin.js
+++ backend/Routes/Admin.js
@@ -2,7 +2,38 @@
 const express = require("express"); 
 const authMiddleware = require("../Middleware/authMiddleware");
 const adminMiddleware = require("../Middleware/adminMiddleware");
-const router = express.Router(); 
+const router = express.Router();   
+const zod = require("zod");
+const User = require("../Model/User");
+const Event = require("../Model/Event");
+
+const eventSchema = zod.object({
+    title:zod.string(), 
+    description:zod.string(), 
+    date:zod.string().date()
+})
+router.post("/event",authMiddleware,adminMiddleware,async(req,res)=>{
+   const eventInput = req.body; 
+   const result = eventSchema.safeParse(eventInput); 
+   if(!result.success){
+     return res.status(400).json({
+        msg:"invalid input"
+     })
+   } 
+   const user = await User.findOne({email:req.email}); 
+   const adminId=user._id;    
+   const event = await Event.create({
+    title:eventInput.title, 
+    description:eventInput.description, 
+    date:eventInput.date, 
+    createdBy:adminId
+   })
+   res.json({
+    msg:"event created successfully", 
+    event
+   })
+
+}) 
 
 router.get("/events",authMiddleware,adminMiddleware,async(req,res)=>{
     return res.json({
