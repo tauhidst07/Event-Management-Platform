09e2d96afecb95e10a47cf224c8317a907fc7f2b
tauhidst07 <atauhid07@gmail.com>
2024-10-21 15:12:01 +0530

    add Event model

---
+++ backend/Model/Event.js
@@ -0,0 +1,23 @@
+
+const mongoose = require("mongoose"); 
+
+const eventSchema = mongoose.Schema({
+    title:{
+        type:String, 
+        required:true
+    },
+    description:{
+        type:String, 
+        required:true
+    }, 
+    date:{
+        type:String, 
+        required:true
+    }, 
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:"User"
+    }
+}) 
+
+module.exports=mongoose.model("Event",eventSchema);
